88146641e96cfd09cbbbc8ec13481a9909cb6ede
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2025-07-02 22:53:15 +0600

    feat: Install react router and configure routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Body from "./Components/Body";
+import Login from "./Components/login";
+import Profile from "./Components/Profile";
+
 function App() {
   return (
     <>
-      <h1 className="font-bold">Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
